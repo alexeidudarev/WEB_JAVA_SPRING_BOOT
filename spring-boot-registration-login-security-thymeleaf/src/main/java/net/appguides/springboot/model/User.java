85b00be84539d9c0a716cf204f4354bcdb68ad31
Alexei Dudarev <alexei.dudarev@gmail.com>
2020-06-27 17:35:52 +0300

    model added

--- spring-boot-registration-login-security-thymeleaf/src/main/java/net/appguides/springboot/model/User.java
+++ spring-boot-registration-login-security-thymeleaf/src/main/java/net/appguides/springboot/model/User.java
@@ -1,5 +1,49 @@
 package net.appguides.springboot.model;
 
-public class User {
+import java.util.Collection;
 
+public class User {
+	private Long id;
+	private String lastName;
+	private String email;
+	private String password;
+	private Collection <Role> roles;
+	public User(String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+	
 }
